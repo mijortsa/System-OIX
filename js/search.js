e5bf73ce989067a9bd47cd98cab321a8b14ed24a
winardiaris <ariswinardy@gmail.com>
2015-05-17 00:14:52 +0700

    list a-z, remove duplicate, enterable

--- js/search.js
+++ js/search.js
@@ -2,11 +2,30 @@
 	jQuery.expr[':'].Contains = function(a,i,m){
       return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
   };
-  function listFilter(header, list) { 
+	// accending sort
+	function asc_sort(a, b){
+		return ($(b).text()) < ($(a).text()) ? 1 : -1;    
+	}
+	// decending sort
+	function dec_sort(a, b){
+		return ($(b).text()) > ($(a).text()) ? 1 : -1;    
+	}
+	//remove duplicate
+	function removeDup(a){
+		var seen = {};
+		$(a).each(function() {
+			var txt = $(this).text();
+				if (seen[txt])
+					$(this).remove();
+				else
+					seen[txt] = true;
+		});
+	}
+	function listFilter(header, list) { 
 	// header is any element, list is an unordered list
    // create and add the filter form to the header
-    var form = $("<form>").attr({"class":"filterform","action":"#"}),
-        input = $("<input>").attr({"class":"filterinput","type":"text","id":"searchinput","placeholder":"Type to Search..."});
+    var form = $("<div>").attr({"class":"filterform"}),
+        input = $("<input>").attr({"class":"filterinput","type":"text","id":"searchinput","placeholder":"Search"});
     $(form).append(input).appendTo(header);
 
      $(input)
@@ -19,30 +38,26 @@
 			if(filter) {
 				// this finds all links in a list that contain the input,
 				// and hide the ones not containing the input while showing the ones that do
-				$(list).find("a:not(:Contains(" + filter + "))").parent().slideUp();	
-				$(list).find("a:Contains(" + filter + ")").parent().slideDown();
-				$(list).show();
-			} 
-			else if(filter==""){
-				$(list).find(".ui-listview-item").slideUp().hide();
+				$(list).find("a:not(:Contains(" + filter + "))").parent().slideUp().hide();
+				$(list).find("a:Contains(" + filter + ")").parent().slideDown().show();
+				$(list).show();				
 			}
-			else{
-				$(list).hide();
-			}
+			else if(filter==""){$(list).find(".ui-listview-item").slideUp().hide();}
+			else{$(list).hide();}
 			return false;
 		})
-		.focusout(function(){
-         
+		.keypress(function(e){
+			if(e.which==13){
+				var a = $(list).find(".ui-listview-item:visible").first().children().attr('desktop');
+				Utils.run_desktop(a);
+			}
 		});
   }
-
   //ondomready
 	$(document).ready(function() {
-		$(document).keydown(function() {
-			$("input").focus();
-		});
+		$(document).keydown(function() {$("input").focus();});
 		listFilter($("#header"), $("#listSearch"));
-		//var ars = $("#listSearch").html();
-		//$("#test").text(ars);
+		$("#listSearch .ui-listview-item").sort(asc_sort).appendTo('#listSearch');
+		removeDup("#listSearch .ui-listview-item");
 	});
 }) (jQuery);
